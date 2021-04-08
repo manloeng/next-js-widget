dd64d77072f7a074e3304d00d2eb2e0f417dcdb1
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-08 20:34:58 +0100

    Add FinancialPlanPage

---
+++ src/components/financialPlanPage/FinancialPlanPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+function FinancialPlanPage() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default FinancialPlanPage;
