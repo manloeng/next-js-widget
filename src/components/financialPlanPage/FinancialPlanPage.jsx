09f530eb3817f95ecd37211cf1ec75b09e908b81
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-08 21:03:19 +0100

    Update PlanPage

--- src/components/financialPlanPage/FinancialPlanPage.jsx
+++ src/components/financialPlanPage/FinancialPlanPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NavBar from "../navbar/NavBar";
+import PersonalFinance from "./widget/PersonalFinance";
+import styles from "../../../styles/FinancialPlanPage.module.css";
 
 function FinancialPlanPage() {
   return (
     <>
       <NavBar></NavBar>
+      <div className={styles.container}>
+        <PersonalFinance></PersonalFinance>
+        <PersonalFinance></PersonalFinance>
+      </div>
     </>
   );
 }
