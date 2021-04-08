55317bb7e18726b1b7bfa9c3e73f06869b46f941
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-08 21:28:04 +0100

    Update form layout

--- src/components/financialPlanPage/widget/PersonalFinance.jsx
+++ src/components/financialPlanPage/widget/PersonalFinance.jsx
@@ -8,7 +8,9 @@ function PersonalFinance() {
 
       <form>
         <div>
-          <label>Annual Income</label>
+          <label>
+            <b>Annual Income</b>
+          </label>
         </div>
         <div>
           <label>Annual Salary:</label>
@@ -20,24 +22,35 @@ function PersonalFinance() {
         </div>
 
         <div>
-          <label>Monthly Spending</label>
+          <label>
+            <b>Monthly Spending</b>
+          </label>
         </div>
 
         <div>
+          {/* could seperate these into mini components */}
           <label>Mortgage:</label>
-          <input type="text" id="salary" name="salary" placeholder="Enter your annual salary..." required />
+          <input
+            type="text"
+            id="mortgage"
+            name="mortgage"
+            placeholder="Enter your monthly mortgage payment..."
+            required
+          />
           <label>From Age:</label>
-          <input type="text" id="fromAge" name="fromAge" placeholder="Enter age..." required />
+          <input type="text" id="mortgageFromAge" name="mortgageFromAge" placeholder="Enter age..." required />
           <label>To Age:</label>
-          <input type="text" id="toAge" name="toAge" placeholder="Enter age..." required />
+          <input type="text" id="mortgageToAge" name="mortgageToAge" placeholder="Enter age..." required />
 
+          <br />
           <label>Bills:</label>
-          <input type="text" id="salary" name="salary" placeholder="Enter your annual salary..." required />
+          <input type="text" id="bills" name="bills" placeholder="Enter your monthly bills..." required />
           <label>From Age:</label>
-          <input type="text" id="fromAge" name="fromAge" placeholder="Enter age..." required />
+          <input type="text" id="billsFromAge" name="billsFromAge" placeholder="Enter age..." required />
           <label>To Age:</label>
-          <input type="text" id="toAge" name="toAge" placeholder="Enter age..." required />
+          <input type="text" id="billsToAge" name="billsToAge" placeholder="Enter age..." required />
 
+          <br />
           <label>General Spending:</label>
           <input type="text" id="salary" name="salary" placeholder="Enter your annual salary..." required />
           <label>From Age:</label>
