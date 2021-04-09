f2c7f6b2dde8a18161b30aa12be13658bd5981df
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-09 22:21:11 +0100

    Update: SpendLess Component

--- src/components/financialPlanPage/widget/SpendLess.jsx
+++ src/components/financialPlanPage/widget/SpendLess.jsx
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import styles from "../../../../styles/FinancialPlanPage.module.css";
 
 function SpendLess() {
-  const [amount, setAmount] = useState(0);
+  const [mortgage, setMortgage] = useState(0);
+  const [bills, setBills] = useState(0);
+  const [spendings, setSpendings] = useState(0);
   const [isHelpfulEnabled, setIsHelpfulEnabled] = useState(true);
+
+  let total = 0;
+  total = mortgage + bills + spendings;
 
   return (
     <div className={styles.widget}>
@@ -11,19 +16,45 @@ function SpendLess() {
       <p>Try reducing your monthly spending to see how your forecast could improve!</p>
 
       <div className={styles.slidecontainer}>
-        <label>Test</label>
-        <input type="range" min="1" max="100" value="50" className={styles.slider} />
+        <label>Mortgage</label>
+        <input
+          type="range"
+          min="1"
+          max="5000"
+          value={mortgage}
+          className={styles.slider}
+          onChange={(e) => setMortgage(parseInt(e.target.value))}
+        />
+        <output>{mortgage}</output>
       </div>
 
       <div className={styles.slidecontainer}>
-        <input type="range" min="1" max="100" value="50" className={styles.slider} />
+        <label>Bills</label>
+        <input
+          type="range"
+          min="1"
+          max="5000"
+          value={bills}
+          className={styles.slider}
+          onChange={(e) => setBills(parseInt(e.target.value))}
+        />
+        <output>{bills}</output>
       </div>
 
       <div className={styles.slidecontainer}>
-        <input type="range" min="1" max="100" value="50" className={styles.slider} />
+        <label>General Spending</label>
+        <input
+          type="range"
+          min="1"
+          max="5000"
+          value={spendings}
+          className={styles.slider}
+          onChange={(e) => setSpendings(parseInt(e.target.value))}
+        />
+        <output>{spendings}</output>
       </div>
 
-      <p>This means you are saving £{amount} per month!</p>
+      <p>This means you are saving £{total} per month!</p>
 
       <button className={styles.button}>Find Ways To Save</button>
 
