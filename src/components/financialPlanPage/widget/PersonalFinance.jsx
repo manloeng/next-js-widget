6dce166973c6178f4627de336f9f0132c34c10f1
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-09 21:19:30 +0100

    Refactor PersonalFinance Widget

--- src/components/financialPlanPage/widget/PersonalFinance.jsx
+++ src/components/financialPlanPage/widget/PersonalFinance.jsx
@@ -1,50 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MinorDataInputCompoent from "./MinorDataInputComponent";
-
 import styles from "../../../../styles/FinancialPlanPage.module.css";
 
 function PersonalFinance({ data = {}, handleUpdate }) {
-  if (!Object.keys(data).length) return <></>;
-  const { incomes, expenditures } = data;
-
+  const keys = Object.keys(data);
+  if (!keys.length) return <></>;
   return (
     <div className={styles.widget}>
       <h1>Your Income And Spend</h1>
 
       <form>
-        <div>
-          <label>
-            <b>Annual Income</b>
-          </label>
-        </div>
-
-        {incomes.map((income) => {
+        {keys.map((key, financeIndex) => {
           return (
-            <MinorDataInputCompoent
-              title={income.name}
-              amount={income.amount ? income.amount : 0}
-              fromAge={income.from_age ? income.from_age : 0}
-              toAge={income.to_age ? income.to_age : 0}
-              handleUpdate={handleUpdate}
-            ></MinorDataInputCompoent>
-          );
-        })}
+            <>
+              <>
+                {financeIndex === 0 ? (
+                  <label>
+                    <b>Annual Income</b>
+                  </label>
+                ) : (
+                  <label>
+                    <b>Monthly Spending</b>
+                  </label>
+                )}
+              </>
 
-        <div>
-          <label>
-            <b>Monthly Spending</b>
-          </label>
-        </div>
-
-        {expenditures.map((expense) => {
-          return (
-            <MinorDataInputCompoent
-              title={expense.name}
-              amount={expense.amount ? expense.amount : 0}
-              fromAge={expense.from_age ? expense.from_age : 0}
-              toAge={expense.to_age ? expense.to_age : 0}
-              handleUpdate={handleUpdate}
-            ></MinorDataInputCompoent>
+              {data[key].map((finance, index) => (
+                <MinorDataInputCompoent
+                  title={finance.name}
+                  amount={finance.amount ? finance.amount : 0}
+                  fromAge={finance.from_age ? finance.from_age : 0}
+                  toAge={finance.to_age ? finance.to_age : 0}
+                  index={index}
+                  handleUpdate={handleUpdate}
+                ></MinorDataInputCompoent>
+              ))}
+            </>
           );
         })}
       </form>
